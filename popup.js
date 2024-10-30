2e4e7ac0b35e8a151c860bb121ee88f4d1373b8a
Dorae0 <126739831+Dorae0@users.noreply.github.com>
2024-10-30 17:39:03 +0900

    Feat: 입력 받아오기 기능 추가
    console.log로 제목 텍스트 받아오는 부분 확인
    console.log로 메모 텍스트 받아오는 부분 확인
    console.log로 현재 탭 저장 / 전체 탭 저장 선택여부 확인
    
    #7

--- popup.js
+++ popup.js
@@ -1,10 +1,23 @@
 const toggleButton = document.getElementById("toggleButton");
-
-// 초기 상태를 정의
+const submitButton = document.getElementById("submitButton");
+const textInput = document.getElementById("textInput");
+const textArea = document.getElementById("textArea");
+// 초기화
 let isSavingCurrentTab = true;
 
-// 체크박스의 상태 변경 이벤트 리스너 추가
+// 슬라이드 토글 버튼 클릭 시 이벤트 추가
 toggleButton.addEventListener("change", () => {
   // 상태를 반대로 바꿈
   isSavingCurrentTab = !isSavingCurrentTab;
+});
+
+// 저장 버튼 (submitButton) 클릭 시 이벤트 추가
+submitButton.addEventListener("click", () => {
+  const titleText = textInput.value;
+  const memoText = textArea.value;
+
+  // 텍스트, 상태 출력
+  console.log("제목:", titleText);
+  console.log("메모:", memoText);
+  console.log(isSavingCurrentTab ? "현재 탭 저장" : "모든 탭 저장");
 });
