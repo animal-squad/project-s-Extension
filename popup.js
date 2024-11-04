5fa72b2d27ba4b8cdb05ce7c760e31b9d6af8db6
Dorae0 <126739831+Dorae0@users.noreply.github.com>
2024-11-04 15:39:21 +0900

    Feat: Google 로그인 구현
    manifest.json에 권한 추가
    popup.js에 이메일 받아오는 코드 추가
    
    #9

--- popup.js
+++ popup.js
@@ -2,22 +2,35 @@ const toggleButton = document.getElementById("toggleButton");
 const submitButton = document.getElementById("submitButton");
 const textInput = document.getElementById("textInput");
 const textArea = document.getElementById("textArea");
+
 // 초기화
 let isSavingCurrentTab = true;
 
 // 슬라이드 토글 버튼 클릭 시 이벤트 추가
 toggleButton.addEventListener("change", () => {
-  // 상태를 반대로 바꿈
   isSavingCurrentTab = !isSavingCurrentTab;
 });
 
 // 저장 버튼 (submitButton) 클릭 시 이벤트 추가
-submitButton.addEventListener("click", () => {
-  const titleText = textInput.value;
-  const memoText = textArea.value;
+submitButton.addEventListener("click", async () => {
+  try {
+    const titleText = textInput.value;
+    const memoText = textArea.value;
 
-  // 텍스트, 상태 출력
-  console.log("제목:", titleText);
-  console.log("메모:", memoText);
-  console.log(isSavingCurrentTab ? "현재 탭 저장" : "모든 탭 저장");
+    console.log("제목:", titleText);
+    console.log("메모:", memoText);
+    console.log(isSavingCurrentTab ? "현재 탭 저장" : "모든 탭 저장");
+
+    chrome.identity.getProfileUserInfo((info) => {
+      if (chrome.runtime.lastError) {
+        console.error("Error getting user info:", chrome.runtime.lastError);
+        return;
+      }
+
+      const email = info.email;  
+      console.log("User Email:", email);
+    });
+  } catch (error) {
+    console.error("버튼 클릭 중 오류 발생:", error);
+  }
 });
